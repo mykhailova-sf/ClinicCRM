8765c46ca51670407539a0f4c732516ee8edcca6
Sofiia Mikhaylova <sophia04mikhaylova@gmail.com>
2024-06-17 19:15:22 +0300

    Add JavaDoc

---
+++ doc-2/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.somihmih"},{"l":"com.somihmih.db"},{"l":"com.somihmih.er.entity"},{"l":"com.somihmih.er.indexservice"},{"l":"com.somihmih.er.utils"}];updateSearchResults();
